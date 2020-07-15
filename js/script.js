204ebf4d3277c78bb4af9e6b2962824f31ba8ca4
José Antonio Hernández <pepehdezx2@gmail.com>
2020-07-14 19:22:16 -0500

    Version 1

---
+++ js/script.js
@@ -0,0 +1,44 @@
+let post  = document.getElementById("ButtonPost");
+let clear  = document.getElementById("ButtonClear");
+let mark  = document.getElementById("ButtonMark");
+let del  = document.getElementById("ButtonDelete");
+
+post.addEventListener("click",TodoPost);
+clear.addEventListener("click",TodoClear);
+mark.addEventListener("click",TodoMark);
+del.addEventListener("click",TodoDel);
+
+
+function TodoPost(e){
+e.preventDefault();
+// Necesito obtener el valor de la caja
+var todo = document.getElementById("todoText").value;
+var list = document.getElementById("todoList");
+
+let currentListHTML = list.innerHTML;
+list.innerHTML = currentListHTML + '<input type = "checkbox" name="todo" /> ' + todo + '<br>';
+
+}
+
+
+function TodoClear(e){
+    e.preventDefault();
+    var todos = document.getElementsByName("todo");
+    for(var i = 0; i < todos.length; i++){
+        todos[i].checked = false;
+    }
+}
+
+function TodoMark(e){
+    e.preventDefault();
+    var todos = document.getElementsByName("todo");
+    for(var i = 0; i < todos.length; i++){
+        todos[i].checked = true;
+    }
+}
+
+function TodoDel(e){
+    e.preventDefault();
+    var list = document.getElementById("todoList");
+    list.innerHTML = "";
+}
